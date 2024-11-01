6461dd0f1a54a513916607fa298032ba3c45c890
Vann-jok <vann-jok@rambler.ru>
2024-11-02 00:11:45 +0300

    SpringBootLesson3

--- demoGbHomework/src/main/java/com/example/demoGbHomework/controllers/PersonController.java
+++ demoGbHomework/src/main/java/com/example/demoGbHomework/controllers/PersonController.java
@@ -1,12 +1,21 @@
 package com.example.demoGbHomework.controllers;
 
+import com.example.demoGbHomework.model.People;
+import com.example.demoGbHomework.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PersonController {
+    @GetMapping("/user")
+    public User restUser(){
+
+        User user=new User("Ivan",42,"guglik@rambler.ru");
+        return user;
+    }
     @GetMapping("/people")
-    public String person(){
-        return "person";
+    public People restPeople(){
+        return new People(1,"Ivan");
     }
 }
---
+++ demoGbHomework/src/main/java/com/example/demoGbHomework/model/People.java
@@ -0,0 +1,27 @@
+package com.example.demoGbHomework.model;
+
+public class People {
+    int id;
+    String name;
+
+    public People(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
